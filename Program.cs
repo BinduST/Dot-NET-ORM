3abc4efb2d8ae48b453b4b7ea34699c30de29485
Bindu S <bindus@thoughtworks.com>
2020-04-13 13:13:20 +0530

    add gitignore and initialize a Hello world app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dot_net_orm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
